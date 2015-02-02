f53b19c7b15e5ced2ee177b670597981531af8be
TomVanBoghout <tvboghout@gmail.com>
2015-02-02 14:02:16 +0100

    Windows adapatation

---
+++ src/wp8/PhoneSettings.cs
@@ -0,0 +1,52 @@
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace Cordova.Extension.Commands {
+    public class PhoneSettings : BaseCommand {
+        public void isPhone(string options) {
+            try {
+                
+            } catch (System.Exception) {
+                // TVB - Return error
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, "PhoneSettings.isPhone error"));
+            }
+        }
+		
+		public void isTablet(string options) {
+            try {
+                
+            } catch (System.Exception) {
+                // TVB - Return error
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, "PhoneSettings.isTablet error"));
+            }
+        }
+		
+		public void getPhoneNumber(string options) {
+            try {
+                
+            } catch (System.Exception) {
+                // TVB - Return error
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, "PhoneSettings.getPhoneNumber error"));
+            }
+        }
+		
+		public void fixPortrait(string options) {
+            try {
+                
+            } catch (System.Exception) {
+                // TVB - Return error
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, "PhoneSettings.fixPortrait error"));
+            }
+        }
+		
+		public void allowRotation(string options) {
+            try {
+                
+            } catch (System.Exception) {
+                // TVB - Return error
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, "PhoneSettings.allowRotation error"));
+            }
+        }
+    }
+}

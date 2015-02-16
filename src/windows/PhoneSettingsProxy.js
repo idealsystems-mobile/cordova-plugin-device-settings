e6f4ef90216a14815a9fa5a2ba8a78411ee18322
TomVanBoghout <tvboghout@gmail.com>
2015-02-16 16:22:30 +0100

    Updated universal windows plugin

--- src/windows/PhoneSettingsProxy.js
+++ src/windows/PhoneSettingsProxy.js
@@ -5,21 +5,20 @@ var isThisAPhone = (cordova.platformId == "windows") && WinJS.Utilities.isPhone;
 
 module.exports = {
      getPhoneNumber: function(success, fail, args) {
-        
+         success("");
      },
      isPhone: function(success, fail, args) {
-		
-        succes({isThisAPhone});
+        success(isThisAPhone);
      },
      isTablet: function(success, fail, args) {
-        succes({!isThisAPhone});
+        success(!isThisAPhone);
      },
      fixPortrait: function(success, fail, args) {
-         
+         success(isThisAPhone);
      },
      allowRotation: function(success, fail, args) {
-         
+         success(!isThisAPhone);
      }
 }
 
-require("cordova/exec/proxy").add("SplashScreen", module.exports);
+require("cordova/exec/proxy").add("PhoneSettings", module.exports);
